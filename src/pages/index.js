3218057e7c534d38dc7f296ec2a8fb9bcd653c91
Robert Samuel <robert_samuel97@hotmail.com>
2021-02-26 09:50:59 -0600

    Install the typography plugin in the app

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,14 @@ import Header from "../components/header";
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+    <div style={{ color: 'purple', margin: '3rem auto', maxWidth: 600 }}>
       <Link to="/contact/">Contact</Link>
       <Header headerText="Hello Gatsby!" />
-      <p>What a world.</p>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
